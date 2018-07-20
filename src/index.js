1c9fe31808050618151c4513d31f803f67de3546
Armando Sifuentes <armandosifuentesjr45@gmail.com>
2018-07-19 22:53:25 -0500

    added a ajax request and a formFields function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 /**
  * es6 modules and imports
  */
-import sayHello from './hello';
-sayHello('World');
+import { getMovies, addMovie, deleteMovie, editMovie } from './api';
 
-/**
- * require style imports
- */
-const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
+  // console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+
+    console.log(`id# ${id} - ${title} - rating: ${rating}`);
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+
+function formFields(){
+    const movieTitle = document.querySelector('#movieTitle').value
+    const movieRating = document.querySelector('#movieRating').value
+    console.log(movieTitle, movieRating)
+    //addMovie(movieTitle, movieRating)
+}
+
+document.getElementById('movieSubmitBtn').addEventListener('click', formFields, true)
